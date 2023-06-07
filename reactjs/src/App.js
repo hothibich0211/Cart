1a03b9068a01575eb5b7ab59db600f2cebdb5460
Ho Thi Bich <bich.ho24@student.passerellesnumeriques.org>
2023-06-07 18:00:25 +0700

    finsh

---
+++ reactjs/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+import ShowProduct from './Components/ShowProduct';
+function App() {
+  return (
+    <div className="App">
+      <ShowProduct/>
+    </div>
+  );
+}
+
+export default App;
